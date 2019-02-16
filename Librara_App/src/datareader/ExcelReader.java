fa01c320ade3808e4575a42ca299623fa66c3542
god <god@god-PC>
2019-02-16 19:10:07 +0530

    <automation update><added excel read and write logic>

--- Librara_App/src/datareader/ExcelReader.java
+++ Librara_App/src/datareader/ExcelReader.java
@@ -1,5 +1,41 @@
 package datareader;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class ExcelReader {
+	
+	private static Workbook wb;
+	private static Sheet sh;
+	private static Row row;
+	private static Cell cell;
+	private static FileInputStream fis;
+	private static FileOutputStream fos;
+	
+public static void main(String[] args) throws Exception {
+	
+	fis = new FileInputStream("E:/Selenium/Automation/Librara_App/src/data.xlsx");
+	wb = WorkbookFactory.create(fis);
+	sh = wb.getSheet("Sheet1");
+	int noOfRows = sh.getLastRowNum();
+	System.out.println(noOfRows);
+	
+	row = sh.createRow(1);
+	cell = row.createCell(0);
+	cell.setCellValue("QAV");
+	System.out.println(cell.getStringCellValue());
+	fos = new  FileOutputStream("E:/Selenium/Automation/Librara_App/src/data.xlsx");
+	wb.write(fos);
+	fos.flush();
+	fos.close();
+	System.out.println("done");
+}
 
 }
